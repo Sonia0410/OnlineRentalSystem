a8aa77f1c939ae30deb6e549217fa35dbf488f61
Edwin <edwin172090566@gmail.com>
2021-10-29 16:34:49 +0800

    init file
    also testing the commit

---
+++ src/Member.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Member {
+    
+    public Member() {
+
+    } 
+
+}
---
+++ src/RentalSystem.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class RentalSystem {
+    
+    private static RentalSystem instance = new RentalSystem();
+
+    public static RentalSystem getInstance(){
+        return instance;
+    }
+
+    private RentalSystem(){
+
+    }
+
+}
